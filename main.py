bb270c022c92cf42e5a3646474f7d763751c6d75
Juliya-sun <lyubimova.yulya.29@yandex.ru>
2024-07-24 13:02:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+amount_of_completed_work = 12
+time_to_instale = 1.5
+course_name = "Python"
+time_to_one_task = time_to_instale*60/amount_of_completed_work/60
+print("Курс:", course_name,", всего задач:", amount_of_completed_work, ", затрачено часов:", time_to_instale, ", среднее время выполнения", time_to_one_task, "часа.")
